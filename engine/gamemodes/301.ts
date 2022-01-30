220c96ac300879c32f28489f4f0b126f518f8b63
AVNOV <arthur.viard@ynov.com>
2022-01-30 18:55:16 +0100

    [ADD] Around the world algorithm into gamemodes, need some tests

--- engine/gamemodes/301.ts
+++ engine/gamemodes/301.ts
@@ -96,8 +96,6 @@ class Game301 extends GameMode {
         }
       })
     }
-
-
   }
 }
 
--- engine/gamemodes/around-the-world.ts
+++ engine/gamemodes/around-the-world.ts
@@ -37,6 +37,61 @@ class AroundTheWorld extends GameMode {
   setPlayers(players: Array<string>) {
     this.currentPlayers = players
   }
+
+  public start(playersTab: Array<string>) {
+    this.setStatus("start")
+    this.setPlayers(playersTab)
+    
+    let i = 0;
+    let throwedDart = 1;
+    let finish = false;
+    let playersList = [];
+    const zoneToHit = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+    for (let toList = 0; toList < this.getPlayers().length; toList++) {
+      playersList.push({ name: this.getPlayers()[toList], score: 0 });
+    }
+    console.log(`Le joueur 1 va commencer, ${playersList[i].name} soit prêt`);
+
+    while (!finish) {
+      if (i === (playersList.length + 1)) {
+        i = 0;
+      }
+      console.log('\n');
+      console.log('\n');
+      console.log(`${playersList[i].name} à toi de faire t'es 3 lancés !`);
+      throwedDart = 1;
+      while (throwedDart <= 3) {
+        console.log('')
+        console.log(`${i} tir :`)
+        console.log('Tu tire...')
+        let random = Math.floor(Math.random() * zoneToHit.length);
+        let targetHit = zoneToHit[random];
+        let score = targetHit;
+        if (score == (playersList[i].score + 1)) {
+          console.log(`Bien joué ${playersList[0].name}  as touché la bonne cible.`);
+          playersList[i].score = score;
+          if (playersList[i].score === 20) {
+            console.log(`BRAVO ${playersList[0].name} ! as gagné la partie !`);
+            finish = true;
+            throwedDart = 4;
+            break;
+          }
+          ++i;
+          break;
+        }
+        else if (score != (playersList[i].score + 1)) {
+          if (throwedDart === 3) {
+            console.log(`${playersList[i].name} n'as plus de fléchette et as raté son tour !`);
+            ++i;
+            break;
+          }
+          console.log(`Cible raté, retente ta change ! Tu dois toucher la zone ${playersList[i].score + 1}`);
+          ++throwedDart;
+        }
+      }
+    }
+  }
 }
 
 export { AroundTheWorld }
